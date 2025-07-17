606ec8d260c01a388596ceb7eb3378ac6f6a6ea8
Conor McManamon <c.s.mcmanamon@gmail.com>
2025-07-17 14:51:50 -0400

    Initial basic timer

---
+++ wedding-frontend/src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import Countdown from "./components/Countdown";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <Countdown />
+    </>
+  );
+}
+
+export default App;
---
+++ wedding-frontend/src/components/Countdown.tsx
@@ -0,0 +1,52 @@
+import React, { useEffect, useState } from "react";
+
+const weddingDate = new Date("2026-09-12T17:00:00");
+
+const Countdown = () => {
+  const calculateTimeLeft = () => {
+    // Use plus sign to convert to number for arithmetic
+    const timeToGo = +weddingDate - +new Date();
+
+    let timeLeft = {
+      days: 0,
+      hours: 0,
+      minutes: 0,
+      seconds: 0,
+    };
+
+    if (timeToGo > 0) {
+      timeLeft = {
+        days: Math.floor(timeToGo / (1000 * 60 * 60 * 24)),
+        hours: Math.floor((timeToGo / (1000 * 60 * 60)) % 24),
+        minutes: Math.floor((timeToGo / 1000 / 60) % 60),
+        seconds: Math.floor((timeToGo / 1000) % 60),
+      };
+    }
+    return timeLeft;
+  };
+
+  // Now use math above to render clock
+
+  // Set initial state using function
+  const [timeLeft, setTimeLeft] = useState(calculateTimeLeft());
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setTimeLeft(calculateTimeLeft());
+    }, 1000);
+
+    return () => clearInterval(timer);
+  }, []);
+
+  return (
+    <div>
+      <h2>Countdown!</h2>
+      <p>
+        {timeLeft.days} days, {timeLeft.hours} hours, {timeLeft.minutes}{" "}
+        minutes, {timeLeft.seconds} seconds
+      </p>
+    </div>
+  );
+};
+
+export default Countdown;
